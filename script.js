84098d8d3921161b9e018db1af447d6cf6d92d69
yyura11 <yyuray199@gmail.com>
2025-05-27 01:32:50 +0300

    Оновлено маніфест без ерорів

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(reg => console.log('Service Worker registered:', reg))
-      .catch(err => console.log('Service Worker error:', err));
-  });
-}
-
-
 
 // --- Бургер-меню ---
 const burgerIcon = document.getElementById('burgerIcon');
--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,42 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
+const CACHE_NAME = 'pwa-cache-v1';
+
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
-// Інсталяція сервіс-воркера і кешування необхідних файлів
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(cacheNames.map(name => {
+        if (name !== CACHE_NAME) {
+          return caches.delete(name);
+        }
+      }))
+    )
   );
 });
 
-// Відповідь на запити: спочатку з кешу, потім з мережі
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-// Активація сервіс-воркера та очищення старих кешів (опційно)
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys()
-      .then(keyList =>
-        Promise.all(
-          keyList.map(key => {
-            if (!cacheWhitelist.includes(key)) {
-              return caches.delete(key);
-            }
-          })
-        )
-      )
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      return new Response("Offline і не в кеші", {
+        headers: { "Content-Type": "text/plain" }
+      });
+    })
   );
 });
